54d914a22efb72b4bb94a3b79ebbea0565b62812
Mercy <mercywmuriithi.mm@gmail.com>
2023-09-16 14:38:13 +0300

    while loop done

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,9 @@
 # your code goes here!
+
+def count_down(integer):
+    while integer>0:
+        print(f'{integer} SECONDS(S)')
+        if integer==1:
+            print('Happy New Year')
+        integer-=1
+count_down(integer=20)
